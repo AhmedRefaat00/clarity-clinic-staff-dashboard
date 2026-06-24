b691b6ee9ac4940b9dee212a3721e13d67a17dc9
ahmed refaat <ahmedrefaat00ar@gmail.com>
2026-06-24 06:20:21 +0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Staff } from './pages/staff/staff';
+import { Services } from './pages/services/services';
+import { Availability } from './pages/availability/availability';
+import { Reports } from './pages/reports/reports';
+import { Calendar } from './pages/calendar/calendar';
+import { WalkIn } from './pages/walk-in/walk-in';
+import { Patients } from './pages/patients/patients';
+import { Profile } from './pages/profile/profile';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    { path: 'dashboard', component: Dashboard },
+    { path: 'staff', component: Staff },
+    { path: 'services', component: Services },
+    { path: 'availability', component: Availability },
+    { path: 'reports', component: Reports },
+    { path: 'calendar', component: Calendar },
+    { path: 'walk-in', component: WalkIn },
+    { path: 'patients', component: Patients },
+    { path: 'profile', component: Profile },
+];
---
+++ src/app/pages/walk-in/walk-in.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-walk-in',
+  imports: [],
+  templateUrl: './walk-in.html',
+  styleUrl: './walk-in.css',
+})
+export class WalkIn {}
